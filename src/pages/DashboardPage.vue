f2463ace481aef4d1275152dd8cb3e404cd26117
Ray Luna <leon.luna.ray@gmail.com>
2024-11-25 22:51:25 -0800

    setup recipe store

--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -5,5 +5,14 @@
 </template>
 
 <script setup>
+import { onMounted } from 'vue';
 import { useUserStore } from '@/stores/user';
+import { useRecipeStore } from '@/stores/recipe';
+
+const userStore = useUserStore();
+const recipeStore = useRecipeStore();
+
+onMounted(() => {
+    recipeStore.fetchRecipes();
+});
 </script>
