f4abc1f69dbd93aef279e7356d27bde64e716950
Ray Luna <leon.luna.ray@gmail.com>
2024-11-26 22:12:49 -0800

    dev

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,10 @@
 import { defineConfig } from 'vite'
-<<<<<<< HEAD
-import { fileURLToPath } from 'url'
-import vue from '@vitejs/plugin-vue'
-=======
 import react from '@vitejs/plugin-react-swc'
 import { fileURLToPath } from 'url'
->>>>>>> main
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-  plugins: [vue()],
-=======
   plugins: [react()],
->>>>>>> main
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
