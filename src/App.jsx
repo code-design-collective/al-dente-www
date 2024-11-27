fa653e17ac556fc6e6bbc29136b5936ce4467965
Ray Luna <leon.luna.ray@gmail.com>
2024-11-26 19:50:14 -0800

    react app setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import { useState } from 'react'
-import './css/index.css'
+import '@/assets/css/index.css'
+
+import { Routes, Route } from "react-router-dom"
+
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+import HomePage from '@/pages/HomePage';
+import DashboardPage from '@/pages/DashboardPage';
+import LoginPage from '@/pages/LoginPage';
+import SignupPage from '@/pages/SignupPage';
 
 const App = () => {
   return (
-    <>
-      <h1 className='red-border text-3xl font-bold underline'>Al Dente</h1>
-    </>
+    <div id="react-app" className='relative h-screen flex justify-between flex-col'>
+    <div>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="dashboard" element={<DashboardPage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="signup" element={<SignupPage />} />
+        </Routes>
+      </main>
+    </div>
+    <Footer />
+  </div>
   )
 }
 
