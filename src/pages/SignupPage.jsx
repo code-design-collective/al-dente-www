dc8a5719adcfc83ff2d90e1d989192be968454f8
Ray Luna <leon.luna.ray@gmail.com>
2024-11-29 17:38:18 -0800

    update signup

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
+
 import { userSignup } from '@/api/auth';
 
 const SignupPage = () => {
@@ -7,62 +8,76 @@ const SignupPage = () => {
 
   // State
   const [formData, setFormData] = useState({
-      email: '',
-      password: '',
-      confirmPassword: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
   });
 
   // Methods
   const handleChange = (e) => {
-      setFormData({
-          ...formData,
-          [e.target.name]: e.target.value,
-      });
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
   };
   const handleSubmit = async (e) => {
-      e.preventDefault();
+    e.preventDefault();
 
-      try {
-          if (formData.password !== formData.confirmPassword) {
-              alert('Passwords do not match');
-              setFormData({
-                  ...formData,
-                  password: '',
-                  confirmPassword: '',
-              });
-              return;
-          }
-
-          const response = await userSignup(formData);
-
-          setFormData({
-              email: '',
-              password: '',
-              confirmPassword: '',
-          });
-
-          alert(response.data.message)
-          navigate('/login')
-      } catch (error) {
-          alert(error.response?.data?.error);
+    try {
+      if (formData.password !== formData.confirmPassword) {
+        alert('Passwords do not match');
+        setFormData({
+          ...formData,
+          password: '',
+          confirmPassword: '',
+        });
+        return;
       }
+
+      const response = await userSignup(formData);
+
+      setFormData({
+        email: '',
+        password: '',
+        confirmPassword: '',
+      });
+
+      alert(response.data.message)
+      navigate('/login')
+    } catch (error) {
+      alert(error.response?.data?.error);
+    }
   };
   return (
     <div id="signup" className="flex justify-center items-center">
       <div className="flex flex-col gap-y-[2rem] bg-blue-400 px-[6rem] py-[4rem] rounded-md">
         <h1>Sign Up</h1>
-        <form className="flex-col-1">
+        <form onSubmit={handleSubmit} className="flex-col-1">
           <div className="flex-col-05">
             <label htmlFor="email">Email:</label>
-            <input type="email" id="email" v-model="formData.email" />
+            <input required
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange} />
           </div>
           <div className="flex-col-05">
             <label htmlFor="password">Password:</label>
-            <input type="password" id="password" v-model="formData.password" />
+            <input required
+              type="password" id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange} />
           </div>
           <div className="flex-col-05">
             <label htmlFor="confirmPassword">Confirm Password:</label>
-            <input type="password" id="confirmPassword" v-model="formData.confirmPassword" />
+            <input required
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              value={formData.confirmPassword}
+              onChange={handleChange} />
           </div>
           <button className="mt-[1rem] py-[0.5rem] hover:bg-blue-100/40 border rounded-md" type="submit">Submit</button>
         </form>
